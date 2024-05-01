b46ff9893f1bfe1e4517ab31687973740629bebd
FerencGer <167904266+FerencGer@users.noreply.github.com>
2024-05-01 15:59:25 +0100

    config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,17 @@
-// your config code
+import algosdk from "algosdk";
+
+const algodToken = "a".repeat(64);
+const server: string = "http://localhost";
+const port: string = "4001";
+
+const mnemonic: string = "eagle trip attract arctic window hammer flock naive daughter hawk exile canyon index pact slim sadness smoke involve stuff baby industry calm master absent swarm";
+
+export function getClient(): algosdk.Algodv2 {
+    let client = new algosdk.Algodv2(algodToken, server, port);
+    return client;
+}
+
+export function getAccount(): algosdk.Account {
+    let account = algosdk.mnemonicToSecretKey(mnemonic);
+    return account;
+}// your config code
