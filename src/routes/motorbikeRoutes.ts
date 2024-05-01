0ecf55d27440c5882e9273b35829e4f772b11421
FerencGer <167904266+FerencGer@users.noreply.github.com>
2024-05-01 15:31:44 +0100

    motorbikeRoutes.ts

---
+++ src/routes/motorbikeRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { validateMotorbikeName } from "../middleware/validators.js";
+import { getMotorbikeData } from "../controllers/motorbikeController.js";
+
+// We will create a router object
+const router = express.Router();
+
+// We will create a route for the car data based on the manufacturer name
+router.get("/:manufacturer", validateMotorbikeName, getMotorbikeData)
+
+// We will export the router
+export default router;
